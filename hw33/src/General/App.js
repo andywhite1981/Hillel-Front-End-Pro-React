556547fba9e2e0fdc3403df829067604ec641c08
andywhite1981 <andrey.travenko@gmail.com>
2023-04-20 13:03:36 +0300

    added PlayerPreview componentt with results and children, added  winner, loser, error and loading states

---
+++ hw33/src/General/App.js
@@ -0,0 +1,39 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Home from './Home';
+import Popular from '../components/Popular/index';
+import Battle from '../components/Battle/index';
+import Results from '../components/Battle/Results';
+import Nav from './Nav';
+
+const router = createBrowserRouter([
+    {
+        element: <Nav />,
+        children: [
+            {
+                path: '/',
+                element: <Home />,
+            },
+            {
+                path: 'popular',
+                element: <Popular />,
+            },
+            {
+                path: 'battle',
+                element: <Battle />,
+            },
+            {
+                path: 'battle/results',
+                element: <Results />,
+            },
+            {
+                path: '*',
+                element: <h1>Error</h1>,
+            },
+        ],
+    },
+]);
+
+const App = () => <RouterProvider router={router} />;
+
+export default App;
---
+++ hw33/src/components/Popular/index.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react';
+import { fetchPopularRepos } from '../api';
+import SelectedLanguage from './SelectedLanguage';
+import Repos from './Repos';
+import { useSearchParams } from 'react-router-dom';
+
+const Popular = () => {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [selectedLanguage, setSelectedLanguage] = useState(searchParams.get('language') || 'All');
+    const [repos, setRepos] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setLoading(true);
+        fetchPopularRepos(selectedLanguage)
+            .then((data) => setRepos(data))
+            .catch((error) => setError(error))
+            .finally(() => setLoading(false));
+        setSearchParams({ language: selectedLanguage });
+    }, [selectedLanguage, setSearchParams]);
+
+    useEffect(() => {
+        const language = searchParams.get('language');
+        setSelectedLanguage(language);
+    }, [searchParams]);
+
+    if (error) {
+        return 'Error';
+    }
+
+    return (
+        <>
+            <SelectedLanguage
+                selectedLanguage={selectedLanguage}
+                setSelectedLanguage={setSelectedLanguage}
+                loading={loading}
+            />
+
+            {!loading ? <Repos repos={repos} /> : <h1 className='home-container'>Loading ...</h1>}
+        </>
+    );
+};
+
+export default Popular;
